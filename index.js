06db4f2900df13bf8accb7319e08a9c69f948d90
iness2 <ines600@yahoo.com>
2022-02-16 10:16:41 +0100

    push

---
+++ index.js
@@ -0,0 +1,66 @@
+$("#form_control").on('submit', function(e) {
+    e.preventDefault();
+
+    var fileList = [];
+    var fileInput = document.getElementById('_files');
+    fileInput.addEventListener('change', function() {
+        fileList = [];
+        for (let i = 0; i < fileInput.files.length; i++) {
+            fileList.push(fileInput.files[i]);
+        }
+    });
+    fectchSaveFiles();
+});
+
+function fectchSaveFiles() {
+    let authorized_format_file = [
+        "image/jpeg",
+        "image/jpg"
+    ];
+    if (fileList.length < 1) {
+        alert('Add a picture to upload');
+        return false;
+    }
+    if (fileList.length > 3) {
+        alert('You can upload a maximum of 3 files.');
+        return false;
+    }
+
+    let isImageFile = ture;
+    fileList.forEach(function(file) {
+        if (authorized_format_file.includes(file.type)) {
+            saveFiles(file);
+        } else {
+            alert('Authorized formats are jpeg & jpg only');
+            isImageFile = false;
+        }
+    });
+    return isImageFile;
+}
+
+function saveFiles(file) {
+    var formData = new FormData();
+    formData.set('file', file);
+    formData.set('file_name', file.name);
+
+    $.ajax({
+        type: "post",
+        url: "/run.php",
+        data: formData,
+        dataType: "text",
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function(response) {
+            response = JSON.parse(response);
+            if (response.error !== undefined) {
+                return false;
+            }
+            let message = response[0] ? response[0] : "";
+            $("#response").append(message);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+}
---
+++ run.php
@@ -0,0 +1,55 @@
+<?php 
+
+function _addError(){
+    $responses['error'] = 'true';
+    print json_encode($responses);
+    exit;
+}
+
+define('IS_AJAX', isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED']) == 'xmlhttprequest');
+
+if(!IS_AJAX){
+    die('Restricted access');
+}
+
+$file = isset($_FILES['file']['tmp_name']) ? $_FILES['file']['tmp_name'] : '';
+$responses = ['error' => 'false'];
+$file_name = $_POST['file_name'];
+
+if(isset($_POST['file'])){
+    if($_POST['file'] === "undefined"){
+        $responses[] = 'nonewfile';
+    }
+}
+
+if($file !== ''){
+    if(0< $_FILES['file']['error']){
+        _addError();
+        $responses[] = 'Upload error';
+    } else {
+        $authorized_format_file = [
+            "image/jpeg",
+            "image/jpg"
+        ];
+        if(!in_array($_FILES['file']['type'], $authorized_format_file)){
+            $responses[] = 'Invalid file type';
+            _addError();
+        }
+
+        while(is_dir($folder_user)){
+            $folder_user = 'vds'.((string) rand(10000, 990000).'_'.time());
+        }
+
+        $create_dir = mkdir($folder_user, 0755);
+
+        if(move_uploaded_file($_FILES['file']['tmp_name'], $folder_user.'/'.$file_name)){
+            $responses[] = 'Converted successfully';
+        } else $responses[] = 'Converted with errors';
+    }
+}
+
+if($responses['error'] = 'false'){
+    unset($responses['error']);
+}
+
+print json_encode($responses);
